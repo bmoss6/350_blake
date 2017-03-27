259b472aaab83de9ac67f2f4c7debe6366916adb
cameron <cameron.meherg@gmail.com>
2017-03-27 14:09:57 -0600

    Removed Mongoose and added a package.json for git init

--- db.js
+++ db.js
@@ -1,8 +1,1 @@
 'use strict';
-// import mongo
-var mongoose = require('mongoose');
-// get our connection to the database
-var db= mongoose.connect('mongodb://localhost/test');
-
-exports.db = db;
-exports.mongoose = mongoose;
